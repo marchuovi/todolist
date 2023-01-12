af6961bb171631cf57196f790a85cb18641f8161
LilianaDal <116796494+LilianaDal@users.noreply.github.com>
2023-01-12 14:43:58 +0100

    btndelete

--- src/components/BtnDelete.jsx
+++ src/components/BtnDelete.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import { Button } from '@mui/material'
-const BtnDelete = () => {
 
-    
+const BtnDelete = () => {
+const url = "http://localhost:3000/tasks";
+    const handleDelete = async(list) => {
+     const response = await axios.delete(url)
+     const updateList = list.filter(item => !item.done);
+    setList(updateList);
+}
+
   return (
-    <div><Button variant="contained" sx={{backgroundColor:'#B4ACE7', ':hover':{bgcolor:"#9486ED"}}} >Delete</Button></div>
+    <div>
+        <Button variant="contained" sx={{backgroundColor:'#B4ACE7', ':hover':{bgcolor:"#9486ED"}}} onClick={handleDelete} >Delete</Button>
+    </div>
+
   )
 }
 
