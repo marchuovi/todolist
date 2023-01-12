a30bf2af727d6276ff7fdc76feea3730aa913e7b
maricarmen <mchuecooviedo@gmail.com>
2023-01-12 10:25:12 +0100

    homesection

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import BtnAdd from './components/BtnAdd'
 import Header from './components/Header'
+import Homesection from './components/Homesection'
 
 const App = () => {
   return (
---
+++ src/components/Homesection.jsx
@@ -0,0 +1,14 @@
+
+import CodeOffIcon from '@mui/icons-material/CodeOff'
+
+const Homesection = () => {
+  return (
+    <div>
+      <p className="sloganHome">Añade lo que quieres aprender a tu lista de tareas Learning</p>
+       <CodeOffIcon></CodeOffIcon>
+      <p>Learning forevery</p>
+    </div>
+  )
+}
+
+export default Homesection
