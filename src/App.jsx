2e22b031df1684ced7ddeabc94bd20f322826862
LilianaDal <116796494+LilianaDal@users.noreply.github.com>
2023-01-12 11:20:49 +0100

    crear btndelete

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import BtnAdd from './components/BtnAdd'
 import Header from './components/Header'
 import { Container } from '@mui/system'
+import BtnDelete from './components/BtnDelete'
 
 const App = () => {
   return (
     <>
       {/* <BtnAdd/> */}
       <Header />
+      {/* <BtnDelete/> */}
     </>
   )
 }
---
+++ src/components/BtnDelete.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Button } from '@mui/material'
+const BtnDelete = () => {
+
+    
+  return (
+    <div><Button variant="contained" sx={{backgroundColor:'#B4ACE7', ':hover':{bgcolor:"#9486ED"}}} >Delete</Button></div>
+  )
+}
+
+export default BtnDelete
