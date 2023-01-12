9f3cdbb8f67ed1e539b01e3790e1a4af041174e5
LilianaDal <116796494+LilianaDal@users.noreply.github.com>
2023-01-12 21:54:05 +0100

    nuevo commit

--- src/components/BtnDelete.jsx
+++ src/components/BtnDelete.jsx
@@ -1,20 +1,26 @@
 import React from 'react'
 import { Button } from '@mui/material'
+import axios from "axios";
 
 const BtnDelete = () => {
 const url = "http://localhost:3000/tasks";
-    const handleDelete = async(list) => {
-     const response = await axios.delete(url)
-     const updateList = list.filter(item => !item.done);
-    setList(updateList);
-}
+
+    const handleDelete = async() => {
+        const updateList = list.filter(item => !item.done);
+        setList(updateList);
+      };
+
+    //  const response = await axios.delete(url)
+    // //  .then((response)=>{setUpdateList(!updateList)}
+    // //  const updateList = list.filter(item => !item.done);
+    // setUpdateList(updateList);
+// }
 
   return (
     <div>
-        <Button variant="contained" sx={{backgroundColor:'#B4ACE7', ':hover':{bgcolor:"#9486ED"}}} onClick={handleDelete} >Delete</Button>
+        <Button variant="contained" sx={{backgroundColor:'#B4ACE7', ':hover':{bgcolor:"#9486ED"}}} >Delete</Button>
     </div>
-
   )
-}
+};
 
 export default BtnDelete
--- src/components/NewList.jsx
+++ src/components/NewList.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Box } from "@mui/system";
+import BtnDelete from "./BtnDelete";
 
 const NewList = () => {
   const url = "http://localhost:3000/tasks";
@@ -16,21 +17,29 @@ const NewList = () => {
       setList(response.data);
     });
   }, []);
+
+  const handleDelete = async() => {
+    const updateList = list.filter(item => !item.done);
+    setList(updateList);
+  };
   console.log(list);
   return (
-    <Box sx={{backgroundColor:'red'}}>
-      {list.map((t, index) => (
-        <p key={index}>{t.task}</p>
-        // <div
-        //   key={index}
-        //   task={task}
-        //   setUpdateList={setUpdateList}
-        //   updateList={updateList}
-        // >
-          
-        // </div>
-      ))}
-    </Box>
+    <>
+      <Box sx={{ backgroundColor: 'red' }}>
+        {list.map((t, index) => (
+          <p key={index}>{t.task}</p>
+          // <div
+          //   key={index}
+          //   task={task}
+          //   setUpdateList={setUpdateList}
+          //   updateList={updateList}
+          // >
+
+          // </div>
+        ))}
+      </Box>
+      <BtnDelete onClick={handleDelete} />
+    </>
   );
 };
 
