5ae2860f645176bca27c4d73394a76f96ad11770
rcarabal2022 <raquelcaraballospain@gmail.com>
2023-01-12 13:16:26 +0100

    >Actualizar list Visualmente

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import BtnAdd from './components/BtnAdd'
 import CheckB from './components/CheckB'
 import Header from './components/Header'
 import { Container } from '@mui/system'
+import NewList from './components/NewList'
 
 const App = () => {
   return (
     <>
       {/* <BtnAdd/> */}
       <Header />
+      <NewList />
     </>
   )
 }
--- src/components/NewList.jsx
+++ src/components/NewList.jsx
@@ -1,42 +1,36 @@
-import React from 'react'
-import CheckB from './CheckB'
+import React, { useEffect, useState } from "react";
+import CheckB from "./CheckB";
+import axios from "axios";
+import { Box } from "@mui/system";
 
-const NewList = props => {
-    const { list, setList } = props;
-    const onChangeStatus = e => {
-        const { name, checked } = e.target;
-
-        const updateList = list.map(item => ({
-          ...item,
-          done: item.id === name ? checked : item.done
-        }));
-        setList(updateList);
-        
-        
-        const onClickRemoveItem = e => {
-            const updateList = list.filter(item => !item.done);
-            setList(updateList);
-          };
-      };
-      const chk = list.map(item => (
-      <CheckB key={item.id} data={item} onChange={onChangeStatus}/>
-    ));
-
-
-    return (
-      <div className="todo-list">
-        {list.length ? chk : "No hay elementos en la lista"}
-        {list.length ? (
-          <p>
-            <button className="button blue">
-              Delete all done
-            </button>
-          </p>
-        ) : null}
-      </div>
-    );
+const NewList = (props) => {
+  const url = "http://localhost:3000/tasks";
+  const getData = async () => {
+    const response = axios.get(url);
+    return response;
   };
 
-export default NewList
+  const [list, setList] = useState([]);
+  const [updateList, setUpdateList] = useState(false)
+  useEffect(() => {
+    getData().then((response) => {
+      setList(response.data);
+    });
+  }, [updateList]);
+  console.log(list);
+  return (
+    <div>
+      {list.map((task, index) => (
+        <div
+          key={index}
+          task={task}
+          setUpdateList={setUpdateList}
+          updateList={updateList}
+        >
+        </div>
+      ))}
+    </div>
+  );
+};
 
-
+export default NewList;
